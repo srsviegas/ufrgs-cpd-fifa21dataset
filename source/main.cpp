c7f41ca9d06cc34775f93988ae92ea25c961d67b
srsviegas <rudiero.viegas@gmail.com>
2023-08-07 18:37:47 -0300

    FEAT: Add main organization

---
+++ source/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "parser.hpp"
+
+void build_structures();
+void start_console();
+
+
+int main() {
+    build_structures();
+    start_console();
+    return 0;
+}
+
+/* Builds the necessary structures for the console mode */
+void build_structures() {
+    return;
+}
+
+/*
+Starts the console mode, when the user is able to input
+the following commands:
+    - player <name|prefix>
+    - user <userID>
+    - top<n> <position>
+    - tags <list of tags>
+    - exit
+*/
+void start_console() {
+    return;
+}
